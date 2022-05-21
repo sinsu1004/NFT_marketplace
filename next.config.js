dfe285bb2789242e4ec419a96b306b897a1e9df4
sinsu <sinsu19012@gmail.com>
2022-05-21 14:19:56 +0900

    LF 수정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 const path = require("path");
 
 module.exports = {
+    
+    images:{
+        loader:'akamai',
+        path:'/',
+
+    },
     reactStrictMode: true,
     sassOptions: {
         includePaths: [path.join(__dirname, "./src/assets/scss")],
